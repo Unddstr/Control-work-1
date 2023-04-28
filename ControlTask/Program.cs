dd7eb3488f09ec8a759569db016680ac5f974c5c
Alexander Panteleev <unddstr@gmail.com>
2023-04-28 16:21:16 +0300

    Функция заполнения нового массива

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -2,3 +2,24 @@
 формирует новый массив из строк, длина которых меньше, либо равна 3 символам*/
 
 string[] arrString = { "1234", "1567", "-2", "computer science", "fox" };
+
+string[] symbol(string[] arrString)
+{
+    int n = 0;
+    for (int i = 0; i < arrString.Length; i++)
+    {
+        if (arrString[i].Length <= 3)
+            n++;
+    }
+    string[] result = new string[n];
+    int j = 0;
+    for (int i = 0; i < arrString.Length; i++)
+    {
+        if (arrString[i].Length <= 3)
+        {
+            result[j] = arrString[i];
+            j++;
+        }
+    }
+    return result;
+}
